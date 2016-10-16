06eb9e89e96afa2b571ba4a6cee2af4c0e45fd12
tafarii <thuntermckenzie@gmail.com>
2016-10-16 20:59:29 +0530

    Implemented a method in the board class to determine if the board is full. In Game, if the board is full, the players will stop alternating. Making the test for the game to end when the board is full pass broke 3 of my of my 5 game tests. The tests wouldn't even run if I didn't include the when then return statement that would end the alternating player loop.
    At this point, working on getting these tests to pass.

--- src/test/java/com/thoughtworks/tafarii/GameTest.java
+++ src/test/java/com/thoughtworks/tafarii/GameTest.java
@@ -78,6 +78,8 @@ public class GameTest {
         verify(secondPlayer).makeMove();
     }
 
+
+
     @Test
     public void shouldStopPlayersFromTakingTurnsWhenTheBoardIsFull() throws Exception {
         //arrange
